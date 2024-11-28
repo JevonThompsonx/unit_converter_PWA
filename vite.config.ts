fd4814f3548b74ccdd4a59cac12218d3e25fe414
Jevon Thompson <jthompsonx00@gmail.com>
2024-11-27 22:06:05 -0500

    updated w/ offline functionality
    - generated icons using vite pwa generator
    - added plugin specifications
    - linked to generated images
    - added vite plugin gen to deps
    - replaced icons in html
    - removed prev manifest link ( auto injected using plugin on prod build)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -8,5 +9,44 @@ export default defineConfig({
   }, preview: {
     port: 8080,
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    injectRegister: 'auto',
+    registerType: 'autoUpdate',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    strategies: 'generateSW',
+    manifest: {
+      name: 'Unit Converter',
+      short_name: 'UnitConv',
+      start_url: "/",
+      description: 'A minimalistic unit converter app',
+      theme_color: '#1F2937',
+      background_color: "#ffffff",
+      display: "standalone",
+      icons: [
+        {
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "maskable-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    }
+  })],
 })
